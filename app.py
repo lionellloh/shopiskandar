eee4f522e5c097b4dd860b6a1db17600fc525a46
lionellloh <lionell_loh@mymail.sutd.edu.sg>
2018-11-16 23:56:27 +0800

    can post now

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, jsonify, request, abort
+import requests
 import json
+import os
 
 app = Flask(__name__)
+
+# Constant variables
+ACCESS_KEY = os.environ['ACCESS_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
+
+FASHION_API = "https://fashion.recoqnitics.com/analyze"
+FACE_API = "https://face.recoqnitics.com/analyze"
+
+data = {'access_key': ACCESS_KEY,'secret_key': SECRET_KEY}
 
 
 @app.route('/find_similar', methods= ['post'])
@@ -10,15 +21,21 @@ def find_similar():
     if not request.json:
         abort(400)
 
+    # Arguments from frontend
     image_string = request.json["image"]
     filter = request.json["filter"]
 
-    print(image_string)
-    print(filter)
+    filename="bryan.jpeg"
+    response = post_data(filename)
 
-    return "Success"
+    return jsonify(response)
 
-
+def post_data(filename):
+    # filename = {'filename': open("test_images/bryan.jpeg", 'rb')}
+    filename = {'filename': open("test_images/{}".format(filename), 'rb')}
+    r = requests.post(FASHION_API, files=filename, data=data)
+    content = json.loads(r.content)
+    return content
 
 
 if __name__ == '__main__':
