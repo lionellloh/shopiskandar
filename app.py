9747f8b0cd4b079391df3da637747b6bacb78095
lionellloh <lionell_loh@mymail.sutd.edu.sg>
2018-11-17 13:46:40 +0800

    fashion_final.csv is cleaned

--- app.py
+++ app.py
@@ -18,17 +18,9 @@ data = {'access_key': ACCESS_KEY,'secret_key': SECRET_KEY}
 
 @app.route('/', methods= ['get'])
 def hello():
-    output = {"url_list": ['https://shop.iskandar.ml/IMAGES/fashion_20.jpg',
-                           'https://shop.iskandar.ml/IMAGES/fashion_4.jpg',
-                           'https://shop.iskandar.ml/IMAGES/fashion_6.jpg',
-                           'https://shop.iskandar.ml/IMAGES/fashion_7.jpg',
-                           'https://shop.iskandar.ml/IMAGES/fashion_18.jpg',
-                           'https://shop.iskandar.ml/IMAGES/fashion_17.jpg',
-                           'https://shop.iskandar.ml/IMAGES/fashion_15.jpg',
-                           'https://shop.iskandar.ml/IMAGES/fashion_1.jpg',
-                           'https://shop.iskandar.ml/IMAGES/fashion_13.jpg']}
 
-    return jsonify(output)
+
+    return "Welcome to the best hackathon in the world!!"
 
 @app.route('/find_similar', methods= ['post'])
 def find_similar():
